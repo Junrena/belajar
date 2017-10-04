3578582933fe82b8a9c3ce1818ca7698520b7b79
Junrena <31508277+Junrena@users.noreply.github.com>
2017-10-05 06:22:58 +0800

    Level 1

---
+++ laracms/app/Http/routes.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function(){
+	//return view('welcome');
+	return "Hi Guys...";
+});
+
+Route::get('/admin/post',function(){
+	return "admin is here.";
+});
+
+Route::get('/about', function(){
+	return "Hi about page";
+});
+
+Route::get('/contact', function(){
+	return "Hi, I am contact";
+});
+
+Route::get('/post/{id}', function($id){
+	return "This is post number " . $id . " " . $name;
+});
+
+Route::get('admin/post/example', array('as'=>admin.home' ,function(){
+	$url = route('admin.home');
+	return "This url is: " . $url;
+}));
+
